1e828a410fc5a88281d17ee060257efd0ee05734
Forrest argzero S <fzs7217@rit.edu>
2017-08-25 14:57:01 -0400

    Initial Commit with Basic Code. Schematics soon.

---
+++ ArduinoLeonardoAsController/ArduinoLeonardoAsController/ArduinoLeonardoAsController.ino
@@ -0,0 +1,50 @@
+#include <Joystick.h>
+
+// Create the Joystick
+Joystick_ Joystick;
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 0;
+const int pinToButtonMap2 = 1;
+
+void setup() {
+  // Initialize Button Pins
+  Serial.begin(9600);
+  //pinMode(pinToButtonMap, INPUT_PULLUP);
+  //pinMode(pinToButtonMap2, INPUT_PULLUP);
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Last state of the button
+float lastButtonState = 0;
+float lastButtonState2 = 0;
+
+void loop() {
+  float currentButtonState = analogRead(pinToButtonMap);
+  if(currentButtonState>500)
+    currentButtonState = 1;
+  else
+    currentButtonState = 0;
+  float currentButtonState2 = analogRead(pinToButtonMap2);
+  if(currentButtonState2>500)
+    currentButtonState2 = 1;
+  else
+    currentButtonState2 = 0;
+  
+  Serial.print(currentButtonState);
+  Serial.print(" "); 
+  Serial.print(currentButtonState2);
+  if (currentButtonState != lastButtonState)
+  {
+    Joystick.setButton(0, currentButtonState);
+    lastButtonState = currentButtonState;
+  }
+  if (currentButtonState2 != lastButtonState2){
+    Joystick.setButton(1, currentButtonState2);
+    lastButtonState2 = currentButtonState2;
+  }
+  Serial.print("\n"); 
+  delay(20);
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+#ARCADE MACHINE HARDWARE CODE
+Make sure to use Arduino IDE ver. 1.8.4+
